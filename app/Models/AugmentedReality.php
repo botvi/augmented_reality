cf904974575b35793539abab9910f6627a6c083b
Yovi Ardiansyah <younoob07mei@gmail.com>
2025-06-14 12:04:40 +0700

    main

--- app/Models/AugmentedReality.php
+++ app/Models/AugmentedReality.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Materi;
 
 class AugmentedReality extends Model
 {
     use HasFactory; 
     protected $table = 'augmented_realities';
     protected $fillable = ['nama_objek', 'objek_3d', 'pattern_pattern', 'pattern_image'];
+
+
+    public function materi()
+    {
+        return $this->belongsTo(Materi::class, 'materi_id');
+    }
 }
